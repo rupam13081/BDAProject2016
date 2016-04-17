1d08e5d29e05361ddf750b483daebc146a920aa7
rupam13081 <rupam13081@iiitd.ac.in>
2016-04-17 23:41:52 +0530

    Update BiclusteringWithSpark.py

--- BiclusteringWithSpark.py
+++ BiclusteringWithSpark.py
@@ -59,14 +59,9 @@ def error(point):
 WSSSE = a.map(lambda point: error(point)).reduce(lambda x, y: x + y)
 print("Within Set Sum of Squared Error = " + str(WSSSE))
 
-# Evaluate clustering by computing Within Set Sum of Squared Errors
-def error(point):
-    center = clusters.centers[clusters.predict(point)]
-    return sqrt(sum([x**2 for x in (point - center)]))
 
-WSSSE = parsedData.map(lambda point: error(point)).reduce(lambda x, y: x + y)
-print("Within Set Sum of Squared Error = " + str(WSSSE))
-
+########Pyspark upto this point
+##SVD unavailable for pyspark
 
 #Bi-Clustering
 from collections import defaultdict
@@ -81,20 +76,19 @@ from scipy.linalg import eigh as largest_eigh
 from sklearn.utils.arpack import eigsh, svds
 from sklearn.utils.validation import assert_all_finite, check_array
 
-n_svd_vecs=None
 
 def _svd(array, n_components, n_discard):
-	u, _, vt = svds(array, k=n_components, ncv=n_svd_vecs)
+	u, _, vt = svds(array, k=n_components, ncv=None)
 	if np.any(np.isnan(vt)):
 		# some eigenvalues of A * A.T are negative, causing
 		# sqrt() to be np.nan. This causes some vectors in vt
 		# to be np.nan.
 		_, v = eigsh(safe_sparse_dot(array.T, array),
-				ncv=n_svd_vecs)
+				ncv=None)
 		vt = v.T
 	if np.any(np.isnan(u)):
 		_, u = eigsh(safe_sparse_dot(array, array.T),
-				ncv=n_svd_vecs)
+				ncv=None)
 	assert_all_finite(u)
 	assert_all_finite(vt)
 	u = u[:, n_discard:]
@@ -117,19 +111,62 @@ def scale_normalize(X):
 	return an, row_diag, col_diag
 
 
-def SpectralCoclustering(X,n_clusters, svd_method,n_svd_vecs, mini_batch, init, n_init, n_jobs, random_state):
+def SpectralCoclustering(X,n_clusters):
 	normalized_data, row_diag, col_diag = scale_normalize(X)
 	n_sv = 1 + int(np.ceil(np.log2(n_clusters)))
 	u, v = _svd(normalized_data, n_sv, n_discard=1)
 	z = np.vstack((row_diag[:, np.newaxis] * u,col_diag[:, np.newaxis] * v))
-
 	model = KMeans.train(z, 2, maxIterations=10,runs=10, initializationMode="random")
-	centroid = model.cluster_centers
 	labels = model.labels_
 	n_rows = X.shape[0]
 	row_labels_ = labels[:n_rows]
 	column_labels_ = labels[n_rows:]
 	rows = np.vstack(row_labels_ == c for c in range(n_clusters))
 	columns = np.vstack(column_labels_ == c for c in range(n_clusters))
-	return -centroid, n_rows,row_labels_,column_labels_,row_labels_,rows,columns,u,v,z,n_sv
+	return -centroid, n_rows,row_labels_,column_labels_,row_labels_,rows,columns
 
+
+cocluster= SpectralCoclustering(X,n_clusters=len(categories),svd_method='arpack', n_svd_vecs=None,mini_batch=False,init='k-means++',n_init=10,n_jobs=1,random_state=0)
+
+feature_names = vectorizer.get_feature_names()
+document_names = list(newsgroups.target_names[i] for i in newsgroups.target)
+
+def bicluster_ncut(i):
+	rows= np.nonzero(cocluster[5][i])[0]
+	cols= np.nonzero(cocluster[6][i])[0]
+	if not (np.any(rows) and np.any(cols)):
+		import sys
+		return sys.float_info.max
+	row_complement = np.nonzero(np.logical_not(cocluster[5][i]))[0]
+	col_complement = np.nonzero(np.logical_not(cocluster[6][i]))[0]
+	weight = X[rows][:, cols].sum()
+	cut = (X[row_complement][:, cols].sum() +X[rows][:, col_complement].sum())
+	return cut / weight
+
+def most_common(d):
+    return sorted(iteritems(d), key=operator.itemgetter(1), reverse=True)
+
+bicluster_ncuts = list(bicluster_ncut(i) for i in range(len(newsgroups.target_names)))
+best_idx = np.argsort(bicluster_ncuts)[:5]
+
+print("Best biclusters:")
+
+for idx, cluster in enumerate(best_idx):
+	n_rows = X.shape[0]
+	n_cols = X.shape[1]
+	cluster_docs, cluster_words = np.nonzero(cocluster[5][cluster])[0],np.nonzero(cocluster[6][cluster])[0]
+	if not len(cluster_docs) or not len(cluster_words):
+		continue
+	counter = defaultdict(int)
+	for i in cluster_docs:
+		counter[document_names[i]] += 1
+	cat_string = ", ".join("{:.0f}% {}".format(float(c) / n_rows * 100, name)for name, c in most_common(counter)[:3])
+	out_of_cluster_docs = cocluster[4] != cluster
+	out_of_cluster_docs = np.where(out_of_cluster_docs)[0]
+	word_col = X[:, cluster_words]
+	word_scores = np.array(word_col[cluster_docs, :].sum(axis=0)-word_col[out_of_cluster_docs, :].sum(axis=0))
+	word_scores = word_scores.ravel()
+	important_words = list(feature_names[cluster_words[i]]for i in word_scores.argsort()[:-11:-1])
+	print("bicluster {} : {} documents, {} words".format(idx, n_rows, n_cols))
+	print("categories   : {}".format(cat_string))
+	print("words        : {}\n".format(', '.join(important_words)))
